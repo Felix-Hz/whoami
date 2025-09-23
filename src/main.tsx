16e15fcbcd8b0e228a37fbadb00755c70338ef8c
Félix Hernández Vieyra <71148989+Felix-Hz@users.noreply.github.com>
2025-09-24 00:15:30 +1200

    feat: biome lint&fmt

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,5 @@ import { render } from "preact";
 import "./index.css";
 import { App } from "./app.tsx";
 
+// biome-ignore lint/style/noNonNullAssertion: preact being preact.
 render(<App />, document.getElementById("app")!);
