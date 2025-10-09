807f44fe0457650d401d5038937d0bdfa866dc61
Félix Hernández Vieyra <71148989+Felix-Hz@users.noreply.github.com>
2025-10-10 00:25:52 +1300

    cicd: add CNAME

--- infra/main.tf
+++ infra/main.tf
@@ -51,17 +51,24 @@ resource "aws_route53_zone" "main" {
 }
 
 # DNS records 
-resource "aws_route53_record" "www" {
+resource "aws_route53_record" "apex" {
   zone_id = aws_route53_zone.main.zone_id
   name    = "felix-hzv.dev"
   type    = "A"
-
 
   alias {
     name                   = aws_cloudfront_distribution.cdn.domain_name
     zone_id                = aws_cloudfront_distribution.cdn.hosted_zone_id
     evaluate_target_health = false
   }
+}
+
+resource "aws_route53_record" "www" {
+  zone_id = aws_route53_zone.main.zone_id
+  name    = "www.felix-hzv.dev"
+  type    = "CNAME"
+  ttl     = 300
+  records = ["felix-hzv.dev"]
 }
 
 resource "aws_cloudfront_origin_access_identity" "origin_access_identity" {
