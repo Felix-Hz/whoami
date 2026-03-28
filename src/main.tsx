7a71d66d6155df6699f06d78306bb6eca4719adb
Felix-Hz <feluhv@outlook.es>
2026-03-28 13:21:41 +1300

    style: blogpost bump

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,10 @@
-import { hydrate } from "preact";
+import { hydrate, render } from "preact";
 import "./index.css";
 import { App } from "./app.tsx";
 
 // biome-ignore lint/style/noNonNullAssertion: preact being preact.
-hydrate(<App />, document.getElementById("app")!);
+const root = document.getElementById("app")!;
+const mount = root.childNodes.length > 0 ? hydrate : render;
+mount(<App />, root);
 
 console.log("%c> you're curious. i like that.", "color: #4a7c59; font-size: 14px; font-family: monospace;");
